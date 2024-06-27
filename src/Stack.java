fcd19d30465375a3913ed3181393aacdd8b1a81a
david <david@Nitishs-MacBook-Air-501.local>
2024-06-27 12:19:03 +0530

    UC2 peak and pop from the Stack till it is empty

--- src/Stack.java
+++ src/Stack.java
@@ -24,33 +24,33 @@ public class Stack<T> {
         newNode.next=top;
         top=newNode;
     }
-//    public T pop(){
-//        if (top==null){
-//            throw new RuntimeException("Stack is empty. Cannot pop.");
-//        }
-//        T data=top.data;
-//        top=top.next;
-//        return data;
-//    }
-//    public T peek(){
-//        if (top==null){
-//            throw new RuntimeException("Stack is empty. Cannot peek.");
-//        }
-//        return top.data;
-//    }
-//    public boolean isEmpty(){
-//        return top==null;
-//    }
-//    public int size(){
-//        if (top==null) return 0;
-//        int c=0;
-//        Node<T> check=top;
-//        while (check!=null){
-//            c++;
-//            check=check.next;
-//        }
-//        return c;
-//    }
+    public T pop(){
+        if (top==null){
+            throw new RuntimeException("Stack is empty. Cannot pop.");
+        }
+        T data=top.data;
+        top=top.next;
+        return data;
+    }
+    public T peek(){
+        if (top==null){
+            throw new RuntimeException("Stack is empty. Cannot peek.");
+        }
+        return top.data;
+    }
+    public boolean isEmpty(){
+        return top==null;
+    }
+    public int size(){
+        if (top==null) return 0;
+        int c=0;
+        Node<T> check=top;
+        while (check!=null){
+            c++;
+            check=check.next;
+        }
+        return c;
+    }
     public void display(){
         if (top==null) System.out.println("Stack is empty");
         Node<T> curr=top;
